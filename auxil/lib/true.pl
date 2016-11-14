99b915004b8f10d55c38eb78e00dcf7d73bf1162
Nicos Angelopoulos <na11@sanger.ac.uk>
2016-11-14 16:55:10 +0000

    FIXED: symbolic link for auxil/lib/true.pl

--- auxil/lib/true.pl
+++ auxil/lib/true.pl
@@ -1,1 +1,17 @@
-../../../../../lib/src/meta/true.pl
+% true( A ).
+%
+%  Always true, deterministically.
+%==
+% ?- true( x ).
+% true.
+%==
+true(_).
+
+% true( A, B ).
+%
+%  Always true, deterministically.
+%==
+% ?- true( x, y ).
+% true.
+%==
+true(_A,_B). % careful, was true(A,A).
